25b1d2b23142d55bbe19f40cdb977490594c3100
Edward Kuenen <edwardkuenen@gmail.com>
2023-05-06 18:37:39 +0200

    Sectie 4, Greetings app

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -1,8 +1,14 @@
 package com.learnkafkastreams.launcher;
 
+import com.learnkafkastreams.topology.GreetingsTopology;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 
 import java.util.List;
 import java.util.Properties;
@@ -13,29 +19,45 @@ public class GreetingsStreamApp {
 
     public static void main(String[] args) {
 
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "greetings-app");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
+        createTopics(properties, List.of(GreetingsTopology.GREETINGS_UPPERCASE, GreetingsTopology.GREETINGS));
+
+        Topology greetingsTopology = GreetingsTopology.buildTopology();
+
+        KafkaStreams kafkaStreams = new KafkaStreams(greetingsTopology, properties);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+        try {
+            kafkaStreams.start();
+
+        } catch (Exception ex) {
+            log.error("Exception in starting stream : {}", ex.getMessage(), ex);
+        }
     }
+
 
     private static void createTopics(Properties config, List<String> greetings) {
 
-        AdminClient admin = AdminClient.create(config);
-        var partitions = 1;
-        short replication  = 1;
+        try (AdminClient admin = AdminClient.create(config)) {
+            int partitions = 2;
+            short replication = 1;
 
-        var newTopics = greetings
-                .stream()
-                .map(topic ->{
-                    return new NewTopic(topic, partitions, replication);
-                })
-                .collect(Collectors.toList());
+            List<NewTopic> newTopics = greetings
+                    .stream()
+                    .map(topic -> new NewTopic(topic, partitions, replication))
+                    .collect(Collectors.toList());
 
-        var createTopicResult = admin.createTopics(newTopics);
-        try {
-           createTopicResult
-                    .all().get();
+            CreateTopicsResult createTopicResult = admin.createTopics(newTopics);
+
+            createTopicResult.all().get();
             log.info("topics are created successfully");
         } catch (Exception e) {
-            log.error("Exception creating topics : {} ",e.getMessage(), e);
+            log.error("Exception creating topics : {} ", e.getMessage(), e);
         }
     }
 }
